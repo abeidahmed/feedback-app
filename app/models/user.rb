6d338b02d7710731a24addba157dcd151c56e78d
abeidahmed <abeidahmed92@gmail.com>
2020-10-09 19:15:27 +0530

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  before_create { email.downcase!}
+
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates_presence_of :email
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_length_of :email, maximum: 255
+  validates_format_of :email, with: VALID_EMAIL_REGEX
+
+  validates_length_of :password, minimum: 5, allow_blank: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  describe 'email validations' do
+    it { should validate_presence_of(:email)}
+
+    it { should validate_uniqueness_of(:email).case_insensitive }
+
+    it { should_not allow_value('abeidmama', 'abeidm@em@.com').for(:email) }
+
+    it { should allow_value('abeidmama@example.com', 'abeidm@em.uk.com').for(:email) }
+
+    it { should validate_length_of(:email).is_at_most(255) }
+
+    it 'is expected to lowercase email before saving' do
+      user = build(:user)
+      user.email = user.email.upcase
+      user.save!
+      expect(user.reload.email).to eq(user.email.downcase)
+    end
+  end
+
+  describe 'password validations' do
+    it { should have_secure_password }
+
+    it { should validate_length_of(:password).is_at_least(5) }
+  end
 end
