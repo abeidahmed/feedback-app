0a65f06879127c4359c21e177408dd74ab1322d0
abeidahmed <abeidahmed92@gmail.com>
2020-10-09 20:32:56 +0530

    add null:false to user email col

--- db/migrate/20201009133444_create_users.rb
+++ db/migrate/20201009133444_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users, id: :uuid do |t|
-      t.string :email
+      t.string :email,          null: false
       t.string :password_digest
 
       t.timestamps
