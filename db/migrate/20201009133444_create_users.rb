0bb7abfb316d57dd3e7a3a6aa020fa7249c1e8e1
abeidahmed <abeidahmed92@gmail.com>
2020-10-09 19:07:28 +0530

    migrate user model

--- db/migrate/20201009133444_create_users.rb
+++ db/migrate/20201009133444_create_users.rb
@@ -6,5 +6,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
 
       t.timestamps
     end
+    add_index :users, :email
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_133052) do
+ActiveRecord::Schema.define(version: 2020_10_09_133444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email"
+  end
 
 end
