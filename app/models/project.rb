8b7e8de592b47ff6bdf17c9eb939a9a5d092a6ec
abeidahmed <abeidahmed92@gmail.com>
2020-10-09 20:38:26 +0530

    add project validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :name
+  validates_length_of :name, maximum: 50
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+
+    it { should validate_length_of(:name).is_at_most(50) }
+  end
+
   describe 'associations' do
     it { should belong_to(:user) }
   end
