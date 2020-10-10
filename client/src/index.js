aa01c097a0744b28dddccaef727f2ce32cc56a0f
abeidahmed <abeidahmed92@gmail.com>
2020-10-10 20:45:02 +0530

    add theme

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import 'assets/stylesheets/font.css';
 import { GlobalStyles } from './globalStyles';
+import { theme } from 'global/theme';
 import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
