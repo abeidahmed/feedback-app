7f63057433f52867b25dc4da761d52922ca51a82
abeidahmed <abeidahmed92@gmail.com>
2020-10-10 21:34:52 +0530

    initialize dashboard header

---
+++ client/src/components/Header/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>hello from header</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
--- client/src/routes/dashboard.js
+++ client/src/routes/dashboard.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from 'containers/Home';
+import { DashboardHeader } from 'components/Header';
 
 function Dashboard() {
   return (
-    <Switch>
-      <Route path="/app" component={Home} />
-    </Switch>
+    <div>
+      <DashboardHeader />
+      <Switch>
+        <Route path="/app" component={Home} />
+      </Switch>
+    </div>
   );
 }
 
