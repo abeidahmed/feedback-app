f7585e43bf9fee048b0a9a67f2195d8901d536b5
abeidahmed <abeidahmed92@gmail.com>
2020-10-11 15:38:11 +0530

    add protected route component

---
+++ client/src/components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { TOKEN } from 'store/currentUser';
+import { Spinner } from 'components/Loader';
+
+function ProtectedRoute({ component: Component, currentUser, ...rest }) {
+  const { token, isLoading, isError } = currentUser;
+
+  if (!Cookies.get(TOKEN) || isError) window.location.href = '/login';
+  if (isLoading) return <Spinner />;
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (token) {
+          return <Component {...props} />;
+        }
+        return (
+          <Redirect
+            to={{
+              pathname: '/login',
+            }}
+          />
+        );
+      }}
+    />
+  );
+}
+
+export default ProtectedRoute;
