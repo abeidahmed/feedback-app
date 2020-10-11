e45f8876f2619f9bca8f45a6a7ef4e1864bbb8b1
abeidahmed <abeidahmed92@gmail.com>
2020-10-11 11:40:15 +0530

    change repeated avatar

--- client/src/containers/Auth/Signup/components/AvatarGroup.js
+++ client/src/containers/Auth/Signup/components/AvatarGroup.js
@@ -26,17 +26,17 @@ function AvatarGroup() {
         />
         <img
           className="inline-block w-8 h-8 -ml-2 text-white rounded-full shadow-solid"
-          src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+          src="https://images.unsplash.com/photo-1591258739299-5b65d5cbb235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
           alt=""
         />
         <img
           className="inline-block w-8 h-8 -ml-2 text-white rounded-full shadow-solid"
-          src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2.25&amp;w=256&amp;h=256&amp;q=80"
+          src="https://images.unsplash.com/photo-1573497019707-1c04de26e58c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
           alt=""
         />
         <img
           className="inline-block w-8 h-8 -ml-2 text-white rounded-full shadow-solid"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+          src="https://images.unsplash.com/photo-1573497491677-1b69852c78a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
           alt=""
         />
       </div>
