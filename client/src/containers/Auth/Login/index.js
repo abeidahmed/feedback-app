c39c73166380c0ea422f5da4da8e3c8f06eacd5a
abeidahmed <abeidahmed92@gmail.com>
2020-10-11 11:42:16 +0530

    add link to logo

--- client/src/containers/Auth/Login/index.js
+++ client/src/containers/Auth/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from 'assets/Logo';
 import { Form, Footer } from './components';
 
@@ -7,9 +8,11 @@ function Login() {
     <main className="flex items-center justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
       <div className="w-full sm:max-w-md">
         <div className="px-4">
-          <Logo width="48" height="48" className="mx-auto" />
+          <Link to="/">
+            <Logo width="48" height="48" className="mx-auto" />
+          </Link>
           <h1 className="text-3xl font-bold text-center sm:text-3xl lg:text-4xl">
-            Sign in to your account
+            Sign in to Feeder
           </h1>
         </div>
         <div className="px-10 py-8 mt-8 bg-white shadow sm:rounded-md">
--- client/src/containers/Auth/Signup/index.js
+++ client/src/containers/Auth/Signup/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from 'assets/Logo';
 import { AvatarGroup, Form, Footer } from './components';
 
@@ -6,7 +7,9 @@ function Signup() {
   return (
     <main className="flex items-center justify-center min-h-screen px-4">
       <div className="w-full">
-        <Logo width="48" height="48" className="mx-auto" />
+        <Link to="/">
+          <Logo width="48" height="48" className="mx-auto" />
+        </Link>
         <section className="w-full py-8">
           <h1 className="text-2xl font-bold text-center sm:text-3xl lg:text-4xl">
             Join Feeder
