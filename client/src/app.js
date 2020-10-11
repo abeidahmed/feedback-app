43a59c9208515322a89e65faeba4c52ffe6110c7
abeidahmed <abeidahmed92@gmail.com>
2020-10-11 15:24:16 +0530

    fetch current user when browser reloads

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { useCurrentUser } from 'store/currentUser';
+import { getCurrentUserApi } from 'api/getCurrentUser';
 import { GuestRoute, DashboardRoute } from 'routes';
+import { Spinner } from 'components/Loader';
 
 function App() {
+  const { setUser, token } = useCurrentUser();
+  const { isLoading, isError } = useQuery(
+    'fetchCurrentUser',
+    getCurrentUserApi,
+    {
+      onSuccess: ({ data }) => {
+        if (token) {
+          setUser(data);
+        }
+      },
+    }
+  );
+
+  if (isError) window.location.href = '/login';
+  if (isLoading) return <Spinner />;
+
   return (
     <Router>
       <Switch>
