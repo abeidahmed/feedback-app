7730e7172c3c5e365b4c774647afb52abd4f19bb
abeidahmed <abeidahmed92@gmail.com>
2020-10-12 21:33:42 +0530

    add get feedbacks query key

--- client/src/global/queryKey.js
+++ client/src/global/queryKey.js
@@ -1,2 +1,3 @@
-export const GET_CURRENT_USER = 'GET_CURRENT_USER';
 export const GET_COLORS = 'GET_COLORS';
+export const GET_CURRENT_USER = 'GET_CURRENT_USER';
+export const GET_FEEDBACKS = 'GET_FEEDBACKS';
