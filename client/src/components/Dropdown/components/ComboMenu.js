04d503bd198b70b145cc03a7e8ea088f27231f2e
abeidahmed <abeidahmed92@gmail.com>
2020-10-12 11:43:28 +0530

    add project link

--- client/src/components/Dropdown/components/ComboMenu.js
+++ client/src/components/Dropdown/components/ComboMenu.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Icon } from 'components/Icon';
 
-function ComboMenu({ icon = 'cog', to, title, ...props }) {
+function ComboMenu({ icon = 'cog', settingLink, tagLink, title, ...props }) {
   return (
     <li className="inline-flex items-center justify-between w-full focus:bg-gray-100 hover:bg-gray-100">
-      <button
+      <Link
+        to={tagLink}
         className="w-full py-2 pl-3 text-left text-gray-700 focus:outline-none focus:bg-gray-100"
         {...props}
       >
         {title}
-      </button>
+      </Link>
       <Link
-        to={to}
+        to={settingLink}
         className="flex px-3 py-2 text-gray-400 hover:text-gray-700 hover:bg-gray-200 focus:outline-none focus:bg-gray-100"
       >
         <Icon icon={icon} className="w-5 h-5" />
