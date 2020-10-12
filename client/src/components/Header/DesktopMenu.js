ccaadbbc9894ef1f87057e4f61a13828c2ced577
abeidahmed <abeidahmed92@gmail.com>
2020-10-12 22:59:58 +0530

    handle logout

--- client/src/components/Header/DesktopMenu.js
+++ client/src/components/Header/DesktopMenu.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { useCurrentUser } from 'store/currentUser';
 
 function DesktopMenu() {
+  const history = useHistory();
+  const { logout } = useCurrentUser();
+
+  function handleLogout() {
+    logout();
+    history.push('/');
+  }
+
   return (
     <nav className="items-center hidden space-x-4 md:flex md:space-x-8">
       <Link to="/" className="text-gray-600 hover:text-gray-900">
@@ -10,9 +19,12 @@ function DesktopMenu() {
       <Link to="/" className="text-gray-600 hover:text-gray-900">
         Help
       </Link>
-      <Link to="/" className="text-gray-600 hover:text-gray-900">
+      <button
+        className="text-gray-600 hover:text-gray-900"
+        onClick={handleLogout}
+      >
         Logout
-      </Link>
+      </button>
     </nav>
   );
 }
