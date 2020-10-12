aaababda929c7afc6ec7302cfc4008ab50539b60
abeidahmed <abeidahmed92@gmail.com>
2020-10-12 15:30:54 +0530

    remove unused test

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,10 +6,6 @@ class Tag < ApplicationRecord
   validates_length_of :name, maximum: 50
   validates_uniqueness_of :name, case_sensitive: false, scope: :project_id
 
-  VALID_COLOR_REGEX = /\A#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/i
-
-  validates_format_of :color, with: VALID_COLOR_REGEX
-
   def is_archive_tag?
     self.name.downcase == 'archive'
   end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -7,10 +7,6 @@ RSpec.describe Tag, type: :model do
     it { should validate_presence_of(:name) }
 
     it { should validate_length_of(:name).is_at_most(50) }
-
-    it { should allow_value("#1f1f1F", "#AFAFAF", "#F00", "#222fff").for :color }
-
-    it { should_not allow_value("#afafah", "#123abce", "#F0h").for :color }
   end
 
   describe 'associations' do
