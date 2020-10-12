d03258f5f1706648d8158d89de68131c925112b3
abeidahmed <abeidahmed92@gmail.com>
2020-10-12 12:53:01 +0530

    remove feedback from api

--- client/src/api/showProject.js
+++ client/src/api/showProject.js
@@ -1,16 +1,6 @@
 import axios from 'axios';
-import qs from 'query-string';
 import { header } from 'utils/header';
 
-export async function showProjectApi(key, { id, filter = '' }) {
-  const url = qs.stringifyUrl(
-    {
-      url: `/v1/projects/${id}`,
-      query: {
-        filter,
-      },
-    },
-    { skipEmptyString: true }
-  );
-  return await axios.get(url, header());
+export async function showProjectApi(key, { id }) {
+  return await axios.get(`/v1/projects/${id}`, header());
 }
