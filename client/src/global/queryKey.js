26f9d1ffddcdfc06cd34bf9cf3ca55fc4bf427c4
abeidahmed <abeidahmed92@gmail.com>
2020-10-13 12:06:46 +0530

    add get tags api

--- client/src/global/queryKey.js
+++ client/src/global/queryKey.js
@@ -2,4 +2,5 @@ export const GET_COLORS = 'GET_COLORS';
 export const GET_CURRENT_USER = 'GET_CURRENT_USER';
 export const GET_FEEDBACKS = 'GET_FEEDBACKS';
 export const GET_PROJECTS = 'GET_PROJECTS';
+export const GET_TAGS = 'GET_TAGS';
 export const SHOW_PROJECT = 'SHOW_PROJECT';
