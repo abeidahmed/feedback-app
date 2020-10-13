68ab8448a63f6df18cebdfbd01e63cf84ba75031
abeidahmed <abeidahmed92@gmail.com>
2020-10-13 19:58:32 +0530

    add prism theme

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'assets/stylesheets/font.css';
 import 'assets/stylesheets/main.css';
+import 'assets/stylesheets/dracula.css';
 import { CurrentUserStore } from 'store/currentUser';
 import App from './app';
 
