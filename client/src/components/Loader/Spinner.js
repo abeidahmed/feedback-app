ca6cde2412e48ad2df23d08eeffd7958ec8a37fd
abeidahmed <abeidahmed92@gmail.com>
2020-10-14 18:57:07 +0530

    use emotion to style spinner

--- client/src/components/Loader/Spinner.js
+++ client/src/components/Loader/Spinner.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import { keyframes } from '@emotion/core';
+import styled from '@emotion/styled';
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const StyledSpinner = styled.div`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+
+  &:after {
+    content: ' ';
+    display: block;
+    width: 40px;
+    height: 40px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 4px solid rgb(0, 93, 255);
+    border-color: rgb(0, 93, 255) transparent rgb(0, 93, 255) transparent;
+    animation: ${spin} 1.2s linear infinite;
+  }
+`;
 
 function Spinner({ ...props }) {
-  return (
-    <div
-      className="absolute transform -translate-x-1/2 -translate-y-1/2 lds-dual-ring left-1/2 top-1/2"
-      {...props}
-    ></div>
-  );
+  return <StyledSpinner {...props}></StyledSpinner>;
 }
 
 export default Spinner;
