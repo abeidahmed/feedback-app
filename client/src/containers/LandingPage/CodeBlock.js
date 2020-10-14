dcd83db4e063c047343c94d680f97ffea2c7dc96
abeidahmed <abeidahmed92@gmail.com>
2020-10-14 21:14:43 +0530

    handle styling with emotion

--- client/src/containers/LandingPage/CodeBlock.js
+++ client/src/containers/LandingPage/CodeBlock.js
@@ -1,20 +1,20 @@
-import React from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import styled from '@emotion/styled';
 import { postCode, CodeBlock as CodeWrapper } from 'components/CodeBlock';
 import { H2, P } from 'components/Typography';
+import { color, media } from 'global/theme';
 
 function CodeBlock() {
   return (
-    <section className="py-10">
+    <Section>
       <div>
         <H2 align="center">Integrating is easy</H2>
         <P size="display" align="center">
           The API is friendly and fast.
         </P>
       </div>
-      <div
-        className="px-4 py-12 mt-8 -mx-4 bg-gray-800 md:px-12 sm:-mx-0"
-        style={{ borderTopRightRadius: 55, borderBottomLeftRadius: 55 }}
-      >
+      <CodeBlockWrapper>
         <div>
           <H2 color="white">Here is the API</H2>
           <P color="white">
@@ -22,12 +22,37 @@ function CodeBlock() {
             rest.
           </P>
         </div>
-        <div className="mt-4">
+        <div
+          css={css`
+            margin-top: 16px;
+          `}
+        >
           <CodeWrapper lang="js" code={postCode} />
         </div>
-      </div>
-    </section>
+      </CodeBlockWrapper>
+    </Section>
   );
 }
+
+const Section = styled.section`
+  padding: 40px 0;
+`;
+
+const CodeBlockWrapper = styled.div`
+  padding: 48px 16px;
+  margin: 32px -16px 0px -16px;
+  background-color: ${color.gray800};
+  border-top-right-radius: 56px;
+  border-bottom-left-radius: 56px;
+
+  ${media.sm`
+    margin-left: 0;
+    margin-right: 0;
+  `}
+
+  ${media.md`
+    padding: 48px;
+  `}
+`;
 
 export default CodeBlock;
