bfe307206351facba0936091c474f2bc34e13466
abeidahmed <abeidahmed92@gmail.com>
2020-10-14 21:07:51 +0530

    update post http code block

--- client/src/components/CodeBlock/postCode.js
+++ client/src/components/CodeBlock/postCode.js
@@ -1,13 +1,15 @@
-export const postCode = `fetch('https://api.feedback.fish/feedback', {
+export const postCode = `fetch('https://feeder.com/v1/projects/:project_id/feedbacks?tag=Issue', {
+  // The "tag" query param is case sensitive. Eg: "Issue", "Idea", "Other", etc..
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
   body: JSON.stringify({
-    projectId: 'ae77fce6b9fb09',
-    text: feedbackText,
-    category: '', // Either "issue", "idea" or "other",
-    userId: currentUser.email,
-    metadata: {},
+    feedback: {
+      content: '', // "The feedback content"
+      sender_email: '', // "The user who gives the feedback"
+      page_url: '', // "/about", "/login", etc..
+      device: '', // "Chrome 64, MacOS", etc..
+    }
   }),
 })`;
