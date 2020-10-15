ae91cc21916e997128b9d46a4ba1000bf044c27f
abeidahmed <abeidahmed92@gmail.com>
2020-10-15 18:28:45 +0530

    add outlineRed variant

--- client/src/global/theme/boxShadow.js
+++ client/src/global/theme/boxShadow.js
@@ -1,6 +1,7 @@
 export const boxShadow = {
   default: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
   outline: 'rgba(0, 93, 255, 0.6) 0px 0px 0px 3px',
+  outlineRed: 'rgba(224, 36, 36, 0.6) 0px 0px 0px 3px',
   lg:
     'rgba(101, 119, 134, 0.2) 0px 0px 15px, rgba(101, 119, 134, 0.15) 0px 0px 3px 1px',
 };
