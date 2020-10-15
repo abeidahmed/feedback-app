2a1b83c7b336399f1a7111df04cbb34e5572e928
abeidahmed <abeidahmed92@gmail.com>
2020-10-15 12:46:03 +0530

    add widget on the landing page

--- client/src/containers/LandingPage/CodeBlock.js
+++ client/src/containers/LandingPage/CodeBlock.js
@@ -1,19 +1,25 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import styled from '@emotion/styled';
+import { color, media } from 'global/theme';
 import { postCode, CodeBlock as CodeWrapper } from 'components/CodeBlock';
 import { H2, P } from 'components/Typography';
-import { color, media } from 'global/theme';
+import { BrandFeedbackForm } from 'components/FeedbackForm';
 
 function CodeBlock() {
   return (
     <Section>
-      <div>
+      <HeadWrapper>
         <H2 align="center">Integrating is easy</H2>
         <P size="display" align="center">
           The API is friendly and fast.
         </P>
-      </div>
+        <Pattern large></Pattern>
+        <Pattern></Pattern>
+        <WidgetWrapper>
+          <BrandFeedbackForm isActive={true} onClose={() => {}} />
+        </WidgetWrapper>
+      </HeadWrapper>
       <CodeBlockWrapper>
         <div>
           <H2 color="white">Here is the API</H2>
@@ -36,16 +42,52 @@ function CodeBlock() {
 
 const Section = styled.section`
   padding: 40px 0;
+  margin: 0 -16px;
+
+  ${media.sm`
+    margin: 0;
+  `}
+`;
+
+const HeadWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+`;
+
+const Pattern = styled.div`
+  background-color: #f96700;
+  position: absolute;
+  bottom: 0;
+  border-top-left-radius: 1000px;
+  border-top-right-radius: 1000px;
+  height: 225px;
+  width: 450px;
+  left: 50%;
+  transform: translateX(-50%);
+
+  ${(props) =>
+    props.large &&
+    css`
+      border-top-left-radius: 1200px;
+      border-top-right-radius: 1200px;
+      height: 285px;
+      width: 600px;
+      background-color: #ffe1cc;
+    `}
+`;
+
+const WidgetWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 48px 0;
 `;
 
 const CodeBlockWrapper = styled.div`
   padding: 48px 16px;
-  margin: 32px -16px 0px -16px;
   background-color: ${color.gray800};
 
   ${media.sm`
-    margin-left: 0;
-    margin-right: 0;
     border-top-right-radius: 56px;
     border-bottom-left-radius: 56px;
 
