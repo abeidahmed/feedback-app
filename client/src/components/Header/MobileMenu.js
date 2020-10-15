f0d33e538a87d0026117ddd543c0ceaab53692f4
abeidahmed <abeidahmed92@gmail.com>
2020-10-15 14:51:18 +0530

    add feedback widget on mobile screens

--- client/src/components/Header/MobileMenu.js
+++ client/src/components/Header/MobileMenu.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import cn from 'classnames';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from '@emotion/styled';
 import { useCurrentUser } from 'store/currentUser';
+import { color, media, boxShadow } from 'global/theme';
+import { BrandFeedbackForm } from 'components/FeedbackForm';
 
 function MobileMenu({ isActive }) {
+  const [feedbackActive, setFeedbackActive] = useState(false);
   const { logout } = useCurrentUser();
   const history = useHistory();
 
@@ -12,32 +15,64 @@ function MobileMenu({ isActive }) {
     history.push('/');
   }
 
-  const mobileMenuClass = cn([
-    'absolute w-full bg-white shadow md:hidden z-10',
-    {
-      block: isActive,
-      hidden: !isActive,
-    },
-  ]);
-
   return (
-    <nav className={mobileMenuClass}>
-      <ul className="w-full py-2">
-        <Link to="/" className="block w-full px-4 py-3">
-          Give your feedback
-        </Link>
-        <Link to="/" className="block w-full px-4 py-3">
-          Help
-        </Link>
-        <button
-          className="block w-full px-4 py-3 text-left"
-          onClick={handleLogout}
-        >
-          Logout
-        </button>
+    <Nav isActive={isActive}>
+      <ul>
+        <WidgetWrapper>
+          <NavButton onClick={() => setFeedbackActive(!feedbackActive)}>
+            Give your feedback
+          </NavButton>
+          <FeedbackWrapper>
+            <BrandFeedbackForm
+              isActive={feedbackActive}
+              onClose={() => setFeedbackActive(false)}
+            />
+          </FeedbackWrapper>
+        </WidgetWrapper>
+        <NavButton onClick={handleLogout}>Logout</NavButton>
       </ul>
-    </nav>
+    </Nav>
   );
 }
+
+const Nav = styled.nav`
+  position: absolute;
+  width: 100%;
+  background-color: #fff;
+  box-shadow: ${boxShadow.default};
+  z-index: 100;
+  display: ${(props) => (props.isActive ? 'block' : 'none')};
+
+  ${media.md`
+    display: none;
+  `}
+
+  > ul {
+    width: 100%;
+    padding: 8px 0;
+  }
+`;
+
+const WidgetWrapper = styled.div`
+  position: relative;
+`;
+
+const FeedbackWrapper = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+const NavButton = styled.button`
+  display: block;
+  width: 100%;
+  padding: 12px 16px;
+  text-align: left;
+
+  &:focus {
+    outline: none;
+    background-color: ${color.gray100};
+  }
+`;
 
 export default MobileMenu;
