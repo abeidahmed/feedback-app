66633811cc414ed66cd593e820a7471869b0dc04
abeidahmed <abeidahmed92@gmail.com>
2020-10-16 21:28:07 +0530

    handle input css with emotion

--- client/src/components/Field/Input.js
+++ client/src/components/Field/Input.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import cn from 'classnames';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import { color, boxShadow, media } from 'global/theme';
 import { ErrorMessage } from './components';
 
 function Input({
@@ -20,41 +22,101 @@ function Input({
     title = to.title;
   }
 
-  const labelClass = cn([
-    'block text-sm font-medium text-gray-700',
-    {
-      'sr-only': !showLabel,
-    },
-  ]);
-
-  const inputClass = cn([
-    'block w-full sm:text-sm form-input focus:border-blue-600 focus:shadow-outline-blue shadow-sm',
-    {
-      'bg-gray-100 focus:bg-white': appearance === 'gray',
-      'mt-1': showLabel,
-    },
-  ]);
-
   return (
     <div>
-      <div className="flex items-center justify-between">
-        <label htmlFor={id} className={labelClass}>
+      <Wrapper>
+        <Label htmlFor={id} showLabel={showLabel}>
           {label}
-        </label>
-        {to && (
-          <Link
-            to={pathname}
-            className="text-sm font-medium text-blue-600 hover:text-blue-500"
-          >
-            {title}
-          </Link>
-        )}
-      </div>
-      <input id={id} className={inputClass} {...props} />
+        </Label>
+        {to && <StyledLink to={pathname}>{title}</StyledLink>}
+      </Wrapper>
+      <StyledInput
+        id={id}
+        showLabel={showLabel}
+        appearance={appearance}
+        {...props}
+      />
       <ErrorMessage error={error} errorType={errorType} />
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Label = styled.label`
+  display: block;
+  font-size: 14px;
+  font-weight: 500;
+  color: ${color.gray700};
+
+  ${(props) =>
+    !props.showLabel &&
+    css`
+      position: absolute;
+      width: 1px;
+      height: 1px;
+      padding: 0;
+      margin: -1px;
+      overflow: hidden;
+      clip: rect(0, 0, 0, 0);
+      white-space: nowrap;
+      border-width: 0;
+    `}
+`;
+
+const StyledInput = styled.input`
+  display: block;
+  width: 100%;
+  font-size: 16px;
+  line-height: 24px;
+  appearance: none;
+  background-color: #fff;
+  border: 1px solid ${color.gray300};
+  border-radius: 0.375rem;
+  padding: 8px 12px;
+  box-shadow: ${boxShadow.sm};
+
+  ${(props) =>
+    props.showLabel &&
+    css`
+      margin-top: 4px;
+    `}
+
+  ${media.sm`
+    font-size: 14px;
+    line-height: 20px;
+  `}
+
+  &:focus {
+    outline: none;
+    border-color: ${color.blue600};
+    box-shadow: ${boxShadow.outline};
+  }
+
+  ${(props) =>
+    props.appearance === 'gray' &&
+    css`
+      background-color: ${color.gray100};
+
+      &:focus {
+        background-color: #fff;
+      }
+    `}
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 14px;
+  font-weight: 500;
+  color: ${color.blue600};
+
+  &:hover {
+    color: ${color.blue500};
+  }
+`;
 
 Input.propTypes = {
   label: PropTypes.string.isRequired,
