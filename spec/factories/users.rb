51d50d1004382a1d47a4a95d22e9a557c71f8f0b
abeidahmed <abeidahmed92@gmail.com>
2020-10-16 17:45:46 +0530

    update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "email#{n}@example.com" }
     password { 'coolpassword' }
+
+    trait :password_reset do
+      password_reset_token { 'helloworld' }
+      password_reset_sent_at { Time.zone.now }
+    end
   end
 end
