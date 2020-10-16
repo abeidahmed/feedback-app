f3683836575c4cae4ac872577bef680a75a17c64
abeidahmed <abeidahmed92@gmail.com>
2020-10-16 19:35:01 +0530

    initialize password reset

---
+++ client/src/containers/PasswordReset/InitiatePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InitiatePage() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default InitiatePage;
--- client/src/routes/guest.js
+++ client/src/routes/guest.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from 'containers/LandingPage';
 import { Signup, Login } from 'containers/Auth';
+import { InitiatePage } from 'containers/PasswordReset';
 
 function Guest() {
   return (
     <Switch>
+      <Route path="/password_reset" component={InitiatePage} />
       <Route path="/signup" component={Signup} />
       <Route path="/login" component={Login} />
       <Route path="/" component={Home} />
