4bd90d33d78d56b11265383147d313335a5405a1
abeidahmed <abeidahmed92@gmail.com>
2020-10-16 21:29:48 +0530

    handle error css with emotion

--- client/src/components/Field/components/ErrorMessage.js
+++ client/src/components/Field/components/ErrorMessage.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { color } from 'global/theme';
 import { findError } from '../utils';
 
 function ErrorMessage({ error, errorType }) {
   if (findError(error, errorType)) {
-    return (
-      <p className="mt-1 text-sm font-medium text-red-700">
-        {findError(error, errorType)}
-      </p>
-    );
+    return <StyledP>{findError(error, errorType)}</StyledP>;
   }
   return null;
 }
+
+const StyledP = styled.p`
+  margin-top: 4px;
+  font-size: 14px;
+  font-weight: 500;
+  color: ${color.red700};
+`;
 
 export default ErrorMessage;
