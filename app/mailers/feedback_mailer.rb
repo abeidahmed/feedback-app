d4f01f517710addb9daa772c63b5886c4be86c42
abeidahmed <abeidahmed92@gmail.com>
2020-10-16 11:45:42 +0530

    remove duplicate class

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,6 +1,4 @@
 class FeedbackMailer < ApplicationMailer
-end
-class FeedbackMailer < ApplicationMailer
   def feedback_mail(feedback:, to_address:)
     @feedback = feedback
     @to_address = to_address
