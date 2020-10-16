628f0d12276252badd26c638080fbbe9ddb1de78
abeidahmed <abeidahmed92@gmail.com>
2020-10-16 11:18:34 +0530

    setup action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -54,6 +54,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "feedback_app_production"
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { host: 'feeder-fish.herokuapp.com' }
+
+  gmail_credentials = Rails.application.credentials.gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => gmail_credentials[:email],
+    :password             => gmail_credentials[:password],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
