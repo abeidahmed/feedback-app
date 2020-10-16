95cc7731d044a4c6e8efb7ebcac23d0de3d3a5a6
abeidahmed <abeidahmed92@gmail.com>
2020-10-16 18:35:34 +0530

    make password reset create action

--- app/controllers/v1/password_resets_controller.rb
+++ app/controllers/v1/password_resets_controller.rb
@@ -1,2 +1,13 @@
 class V1::PasswordResetsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def create
+    user = User.find_by_email(params.dig(:user, :email))
+
+    if user
+      user.initialize_password_reset
+    else
+      render json: { message: 'Invalid email address' }, status: :bad_request
+    end
+  end
 end
--- spec/requests/v1/password_resets_request_spec.rb
+++ spec/requests/v1/password_resets_request_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "V1::PasswordResets", type: :request do
+  describe '#create' do
+    let(:user) { create(:user) }
+    let(:valid_user) { { user: { email: user.email } }.to_json }
+    let(:invalid_user) { { user: { email: 'hello@example.com' } }.to_json }
 
+    context 'when the password request is made by a user' do
+      before do
+        post v1_password_resets_url, params: valid_user, headers: default_header
+      end
+
+      it 'is expected to set password_reset_token with SecureRandom hash' do
+        expect(user.reload.password_reset_token).to be_present
+      end
+
+      it 'is expected to set password_reset_sent_at to current time' do
+        expect(user.reload.password_reset_sent_at).to be_present
+      end
+    end
+
+    context 'when the password request is made by a guest' do
+      before do
+        post v1_password_resets_url, params: invalid_user, headers: default_header
+      end
+
+      it 'is expected to render an error message' do
+        expect(json[:message]).to match(/Invalid email address/)
+      end
+
+      include_examples 'bad_request'
+    end
+  end
 end
