1fca5e9465bdb83f9295a4023f4f780e1d517c68
abeidahmed <abeidahmed92@gmail.com>
2020-10-17 15:20:35 +0530

    add check solid icon

--- client/src/components/Icon/index.js
+++ client/src/components/Icon/index.js
@@ -18,6 +18,15 @@ const Glyph = ({ icon }) => {
           <path d="M5 13l4 4L19 7" strokeLinecap="round" strokeWidth="2" />
         </g>
       );
+    case 'check-solid':
+      return (
+        <g>
+          <path
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+            fillRule="evenodd"
+          />
+        </g>
+      );
     case 'chevron-down':
       return (
         <g>
