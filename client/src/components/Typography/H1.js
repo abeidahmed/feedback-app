8a0f2f82ab65f77cf01a5be18758602ff5265800
abeidahmed <abeidahmed92@gmail.com>
2020-10-17 19:49:03 +0530

    make improvements to h1 component

--- client/src/components/Typography/H1.js
+++ client/src/components/Typography/H1.js
@@ -1,12 +1,11 @@
-import React from 'react';
 import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import { color, media } from 'global/theme';
 
-const StyledH1 = styled.h1`
-  color: ${color.gray800};
+const H1 = styled.h1`
+  color: ${(props) => props.color || color.gray800};
   font-weight: bold;
-  text-align: ${(props) => (props.align === 'center' ? 'center' : 'left')};
+  text-align: ${(props) => props.align || 'left'};
   font-size: 1.5rem;
 
   ${media.sm`
@@ -32,9 +31,5 @@ const StyledH1 = styled.h1`
     `}
     `}
 `;
-
-function H1({ children, ...props }) {
-  return <StyledH1 {...props}>{children}</StyledH1>;
-}
 
 export default H1;
