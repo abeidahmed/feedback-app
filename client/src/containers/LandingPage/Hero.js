1d2401472457a649578edf4268588e4b2741f3c7
abeidahmed <abeidahmed92@gmail.com>
2020-10-17 10:57:44 +0530

    switch to new button component

--- client/src/containers/LandingPage/Hero.js
+++ client/src/containers/LandingPage/Hero.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { useCurrentUser } from 'store/currentUser';
-import { Button } from 'components/Button';
+import { Button } from 'components/Buttons';
 import { Icon } from 'components/Icon';
 import { H1, P } from 'components/Typography';
 
@@ -13,7 +13,11 @@ function Hero() {
       : 'Go to dashboard';
 
   return (
-    <section className="py-10">
+    <section
+      css={css`
+        padding: 40px 0;
+      `}
+    >
       <H1 size="display">
         Get feedback. <br /> Make your customers happy.
       </H1>
@@ -28,30 +32,14 @@ function Hero() {
         Collect issues, ideas and compliments with a simple widget. Receive them
         as email. Dive deep with the dashboard.
       </P>
-      <div className="mt-4">
-        <Button
-          to="/signup"
-          appearance="primary"
-          size="sm"
-          className="rounded-full md:hidden"
-        >
+      <div
+        css={css`
+          margin-top: 16px;
+        `}
+      >
+        <Button to="/signup" color="primary" size="lg" rounded={9999} iconRight>
           {buttonTitle}
-          <Icon
-            icon="chevron-right"
-            className="w-4 h-5 ml-1 -mr-1 text-white"
-          />
-        </Button>
-        <Button
-          to="/signup"
-          appearance="primary"
-          size="md"
-          className="hidden rounded-full md:inline-flex"
-        >
-          {buttonTitle}
-          <Icon
-            icon="chevron-right"
-            className="w-4 h-5 ml-1 -mr-1 text-white"
-          />
+          <Icon icon="chevron-right" className="w-4 h-4 text-white" />
         </Button>
       </div>
     </section>
