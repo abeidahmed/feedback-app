0c434a7780e114479a4ebffce797e40207cc25df
abeidahmed <abeidahmed92@gmail.com>
2020-10-18 10:00:16 +0530

    add md box-shadow variant

--- client/src/global/theme/boxShadow.js
+++ client/src/global/theme/boxShadow.js
@@ -3,6 +3,7 @@ export const boxShadow = {
   outline: 'rgba(0, 93, 255, 0.6) 0px 0px 0px 3px',
   outlineRed: 'rgba(224, 36, 36, 0.6) 0px 0px 0px 3px',
   sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+  md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
   lg:
     'rgba(101, 119, 134, 0.2) 0px 0px 15px, rgba(101, 119, 134, 0.15) 0px 0px 3px 1px',
 };
