525ca9333902b8d72f075cd213da6b8b2e759544
abeidahmed <abeidahmed92@gmail.com>
2020-10-18 10:00:41 +0530

    add margin left prop

--- client/src/components/Typography/P.js
+++ client/src/components/Typography/P.js
@@ -5,6 +5,7 @@ const P = styled.p`
   font-size: ${(props) => props.fontSize || 16}px;
   text-align: ${(props) => props.align || 'left'};
   margin-top: ${(props) => props.marginTop}px;
+  margin-left: ${(props) => props.marginLeft}px;
   color: ${(props) => props.color || color.gray600};
   line-height: ${(props) => props.lineHeight || 24}px;
   font-weight: ${(props) => props.fontWeight || 400};
