5887a6fa3e0dee40f20709c52075bd6ef843cd8f
abeidahmed <abeidahmed92@gmail.com>
2020-10-18 10:00:23 +0530

    add md box-shadow variant

--- client/src/global/theme/index.js
+++ client/src/global/theme/index.js
@@ -2,3 +2,4 @@ export { default as GlobalStyle } from './GlobalStyle';
 export { boxShadow } from './boxShadow';
 export { color } from './color';
 export { media } from './breakpoint';
+export { truncate } from './truncate';
