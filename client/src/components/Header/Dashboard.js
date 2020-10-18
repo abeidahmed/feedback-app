80ede59ad0037061404edeb406ebb78e11f2936a
abeidahmed <abeidahmed92@gmail.com>
2020-10-18 10:36:10 +0530

    handle styling with emotion

--- client/src/components/Header/Dashboard.js
+++ client/src/components/Header/Dashboard.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+/** @jsx jsx */
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import cn from 'classnames';
+import { css, jsx } from '@emotion/core';
+import styled from '@emotion/styled';
 import { useGetProjects } from 'api/allProjects';
+import { media, color } from 'global/theme';
 import { Logo } from 'assets/svg';
 import MobileMenu from './MobileMenu';
 import DesktopMenu from './DesktopMenu';
@@ -14,40 +17,94 @@ function Dashboard() {
 
   const { projects, isLoading, isError } = useGetProjects();
 
-  const overlayClass = cn([
-    'fixed inset-0 md:hidden z-10',
-    {
-      hidden: !isActive,
-      block: isActive,
-    },
-  ]);
-
   return (
-    <header className="relative bg-white">
-      <div className={overlayClass}>
-        <div className="absolute inset-0 bg-gray-300 opacity-75 "></div>
-      </div>
-      <Container className="relative z-10 bg-white">
-        <div className="flex items-center justify-between h-16 border-b border-gray-200">
-          <div className="flex items-center flex-1">
+    <header
+      css={{
+        position: 'relative',
+        backgroundColor: '#fff',
+      }}
+    >
+      <Overlay isActive={isActive}>
+        <div />
+      </Overlay>
+      <Container
+        css={{
+          position: 'relative',
+          zIndex: 10,
+          backgroundColor: '#fff',
+        }}
+      >
+        <HeadWrapper>
+          <div
+            css={{
+              display: 'flex',
+              alignItems: 'center',
+              flex: 1,
+            }}
+          >
             <Link to="/app">
               <Logo width="48" height="48" />
             </Link>
             {isLoading || isError ? null : <TagSelector projects={projects} />}
           </div>
           <DesktopMenu />
-          <div className="flex items-center justify-center ml-3 -mr-1.5 md:hidden">
+          <MenuButton>
             <IconButton
               icon={`${isActive ? 'chevron-up' : 'chevron-down'}`}
               appearance="minimal"
               onClick={() => setIsActive(!isActive)}
             />
-          </div>
-        </div>
+          </MenuButton>
+        </HeadWrapper>
       </Container>
       <MobileMenu isActive={isActive} />
     </header>
   );
 }
+
+const MenuButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 12px;
+  margin-right: -6px;
+
+  ${media.md`
+    display: none;
+  `}
+`;
+
+const HeadWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 64px;
+  border-bottom: 1px solid ${color.gray200};
+`;
+
+const inset = css`
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  ${inset};
+  z-index: 10;
+  display: ${(props) => !props.isActive && 'none'};
+
+  ${media.md`
+    display: none;
+  `}
+
+  > div {
+    position: absolute;
+    ${inset};
+    background-color: ${color.gray300};
+    opacity: 0.75;
+  }
+`;
 
 export default Dashboard;
