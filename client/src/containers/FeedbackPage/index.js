e021e72d8fc905b57c6817df73feb322f6e3b368
abeidahmed <abeidahmed92@gmail.com>
2020-10-19 15:13:57 +0530

    handle styling with emotion

--- client/src/containers/FeedbackPage/index.js
+++ client/src/containers/FeedbackPage/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styled from '@emotion/styled';
 import { useAddQuery } from 'utils/useAddQuery';
 import { useShowProject } from 'api/showProject';
 import { useGetFeedbacks } from 'api/allFeedbacks';
 import { useGetTags } from 'api/getTags';
+import { media } from 'global/theme';
 import { FeedbackCard } from 'components/Card';
 import { Tab } from 'components/Tab';
 import { FilterList, ActionButtonGroup } from './components';
@@ -35,13 +37,13 @@ function FeedbackPage() {
     >
       <ActionButtonGroup project={project} />
       {tagLoading || tagError ? null : <Tab tags={tags} projectId={id} />}
-      <section className="py-4 md:grid md:grid-cols-3 md:gap-6 lg:gap-16">
-        <div className="hidden md:block md:col-span-1">
+      <Section>
+        <FilterWrapper>
           {tagLoading || tagError ? null : (
             <FilterList tags={tags} projectId={id} />
           )}
-        </div>
-        <div className="relative space-y-4 md:col-span-2">
+        </FilterWrapper>
+        <FeedbackWrapper>
           {feedbacksLoading || feedbacksError ? (
             <Spinner />
           ) : (
@@ -53,10 +55,47 @@ function FeedbackPage() {
               />
             ))
           )}
-        </div>
-      </section>
+        </FeedbackWrapper>
+      </Section>
     </PageHeader>
   );
 }
+
+const Section = styled.section`
+  padding: 16px 0;
+
+  ${media.md`
+    display: grid;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    grid-column-gap: 24px;
+    grid-row-gap: 24px;
+  `}
+
+  ${media.lg`
+    grid-column-gap: 64px;
+    grid-row-gap: 64px;
+  `}
+`;
+
+const FeedbackWrapper = styled.div`
+  position: relative;
+
+  > * + * {
+    margin-top: 16px;
+  }
+
+  ${media.md`
+    grid-column: span 2 / span 2;
+  `};
+`;
+
+const FilterWrapper = styled.div`
+  display: none;
+
+  ${media.md`
+    display: block;
+    grid-column: span 1 / span 1;
+  `};
+`;
 
 export default FeedbackPage;
