df3951457b586908a8b63b1b491dd2e6bc1b59bc
abeidahmed <abeidahmed92@gmail.com>
2020-10-19 12:46:59 +0530

    send invited member list

--- client/src/global/theme/index.js
+++ client/src/global/theme/index.js
@@ -1,5 +1,6 @@
 export { default as GlobalStyle } from './GlobalStyle';
 export { boxShadow } from './boxShadow';
 export { color } from './color';
+export { maxWidth } from './width';
 export { media } from './breakpoint';
 export { truncate } from './truncate';
