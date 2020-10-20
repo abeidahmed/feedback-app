b1d0381010ba17ce5d4869c5e7d52cb05c4d6003
abeidahmed <abeidahmed92@gmail.com>
2020-10-20 12:00:10 +0530

    improve feedback mail

--- spec/mailers/previews/feedback_mailer_preview.rb
+++ spec/mailers/previews/feedback_mailer_preview.rb
@@ -1,6 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/feedback_mailer
 class FeedbackMailerPreview < ActionMailer::Preview
-  def feedback_mail(feedback: Feedback.first, to_address: 'hello')
-    FeedbackMailer.feedback_mail(feedback: Feedback.first, to_address: 'hello')
+  def feedback_mail
+    feedback = Feedback.last
+    user = User.first
+    FeedbackMailer.feedback_mail(feedback: feedback, to_address: user.email)
   end
 end
